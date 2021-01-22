a34d10d7d10c527ca9e4af73d98d54894a9f3527
reboot-ua <reboot.if.ua@gmail.com>
2021-01-22 19:16:08 +0200

    54 lesson

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,6 +13,9 @@ const Navbar = () => {
                     <NavLink to='/dialog' activeClassName={classes.active}>Message</NavLink>
                 </div>
                 <div className={classes.item}>
+                    <NavLink to='/users' activeClassName={classes.active}>Users</NavLink>
+                </div>
+                <div className={classes.item}>
                     <NavLink to="/news" activeClassName={classes.active}>News</NavLink>
                 </div>
                 <div className={classes.item}>
