0293dc0918ac8e8acfcbde3f613034e052f487ab
reboot-ua <reboot.if.ua@gmail.com>
2021-01-27 09:29:01 +0200

    71 profileStatus

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import classes from'./ProfileInfo.module.css';
 import Preloader from "../../common/preloader/preloader";
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
         return <Preloader />
     }
     return  <div>
-        <div>
-            <img
-                src="https://media.macphun.com/img/uploads/customer/how-to/579/15349456005b7d69405aabf4.32904503.jpg?q=85&w=1340"
-                alt=""/>
-        </div>
+        {/*<div>*/}
+        {/*    <img*/}
+        {/*        src="https://media.macphun.com/img/uploads/customer/how-to/579/15349456005b7d69405aabf4.32904503.jpg?q=85&w=1340"*/}
+        {/*        alt=""/>*/}
+        {/*</div>*/}
         <div className={classes.descriptionBlock}>
             <img src={props.profile.photos.large}/>
+            <ProfileStatus status={"YO Bro :)"}/>
             ava+discription
         </div>
     </div>
